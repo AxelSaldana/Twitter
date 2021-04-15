24fc624db2aabe70a19f489194ba0ef9f1a68b5e
Forest24 <180606@upslp.edu.mx>
2021-04-15 00:02:24 -0500

    --- Auto-Git Commit ---

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -8,21 +8,31 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var MongoC = conectarBD()
+var MongoCN = ConectarBD()
 var clientOptions = options.Client().ApplyURI("mongodb+srv://axel:123456789@twitter.acy6k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-func ConectarBD() *mongo.ClientOptions {
+/*ConectarBD es la funcion que me permite conecetar la BD*/
+func ConectarBD() *mongo.Client {
 
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 		return client
 	}
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 		return client
 	}
-	log.Printfln("Conexion exitosa con la BD")
+	log.Println("Conexion exitosa con la BD")
 	return client
+}
+
+/*Chequeo connection es el ping de la BD */
+func ChequeoConnection() int {
+	err := client.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
 }
