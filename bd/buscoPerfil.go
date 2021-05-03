a48e903349ef9db6f05145fd8a6c828317b8dc5a
Forest24 <180606@upslp.edu.mx>
2021-05-03 02:53:56 -0500

    verPerfil

--- bd/buscoPerfil.go
+++ bd/buscoPerfil.go
@@ -13,17 +13,13 @@ import (
 func BuscoPerfil(ID string) (models.Usuario, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
-
 	db := MongoCN.Database("twitter")
 	col := db.Collection("usuarios")
-
 	var perfil models.Usuario
 	objID, _ := primitive.ObjectIDFromHex(ID)
-
 	condicion := bson.M{
 		"_id": objID,
 	}
-
 	err := col.FindOne(ctx, condicion).Decode(&perfil)
 	perfil.Password = ""
 	if err != nil {
