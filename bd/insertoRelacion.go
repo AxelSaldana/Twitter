4585c6ee61017c75093e7bdaf7d3a004063726f4
Forest24 <180606@upslp.edu.mx>
2021-05-10 03:51:41 -0500

    --- Auto-Git Commit ---

---
+++ bd/insertoRelacion.go
@@ -0,0 +1,24 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/Forest24/Twitter/models"
+)
+
+/*InsertoRelacion graba la relación en la BD */
+func InsertoRelacion(t models.Relacion) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	col := db.Collection("relacion")
+
+	_, err := col.InsertOne(ctx, t)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -21,6 +21,13 @@ func Manejadores() {
 	router.HandleFunc("/tweet", middlew.ChequeoBD(middlew.ValidoJWT(routers.GraboTweet))).Methods("POST")
 	router.HandleFunc("/leoTweets", middlew.ChequeoBD(middlew.ValidoJWT(routers.LeoTweets))).Methods("GET")
 	router.HandleFunc("/eliminarTweet", middlew.ChequeoBD(middlew.ValidoJWT(routers.EliminarTweet))).Methods("DELETE")
+
+	router.HandleFunc("/subirAvatar", middlew.ChequeoBD(middlew.ValidoJWT(routers.SubirAvatar))).Methods("POST")
+	router.HandleFunc("/obtenerAvatar", middlew.ChequeoBD(routers.ObtenerAvatar)).Methods("GET")
+	router.HandleFunc("/subirBanner", middlew.ChequeoBD(middlew.ValidoJWT(routers.SubirBanner))).Methods("POST")
+	router.HandleFunc("/obtenerBanner", middlew.ChequeoBD(routers.ObtenerBanner)).Methods("GET")
+
+	router.HandleFunc("/altaRelacion", middlew.ChequeoBD(middlew.ValidoJWT(routers.AltaRelacion))).Methods("POST")
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8080"
