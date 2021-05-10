967184f7e87d6924dc0bba6430f8b0b6bb6bc2a8
Forest24 <180606@upslp.edu.mx>
2021-05-10 00:54:00 -0500

    verPerfil

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -17,6 +17,7 @@ func Manejadores() {
 	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlew.ChequeoBD(routers.Login)).Methods("POST")
 	router.HandleFunc("/verperfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.VerPerfil))).Methods("GET")
+	router.HandleFunc("/modificarPerfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.ModificarPerfil))).Methods("PUT")
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8080"
