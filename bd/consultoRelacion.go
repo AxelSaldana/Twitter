5d8f85c56e6ef37ab7a9a884fba3ee93a2fa414a
Forest24 <180606@upslp.edu.mx>
2021-05-10 16:36:18 -0500

    --- Auto-Git Commit ---

---
+++ bd/consultoRelacion.go
@@ -0,0 +1,30 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/Forest24/Twitter/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+/*ConsultoRelacion consulta la relacion entre 2 usuarios */
+func ConsultoRelacion(t models.Relacion) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	col := db.Collection("relacion")
+
+	condicion := bson.M{
+		"usuarioid":         t.UsuarioID,
+		"usuariorelacionid": t.UsuarioRelacionID,
+	}
+
+	var resultado models.Relacion
+	err := col.FindOne(ctx, condicion).Decode(&resultado)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
